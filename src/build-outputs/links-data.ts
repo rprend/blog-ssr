902ebc61f22a274a419deb394ba884e12c933fb8
Ryan Prendergast <rprendergast1121@gmail.com>
2026-03-30 20:29:38 -0400

    Add RLM CLI linkpost

--- src/build-outputs/links-data.ts
+++ src/build-outputs/links-data.ts
@@ -11,6 +11,14 @@ export interface LinkEntry {
 
 export const linkEntries: LinkEntry[] = [
   {
+    "slug": "rlmCli",
+    "title": "RLM CLI",
+    "url": "https://github.com/viplismism/rlm-cli",
+    "date": "03-30-2026",
+    "image": null,
+    "content": "<p>RLM CLI. I had the thought to make this myself two weeks ago and thought id do it later. Goes to show that being quick is important</p>\n"
+  },
+  {
     "slug": "asterismBooks",
     "title": "Asterism Books",
     "url": "https://asterismbooks.com/",
---
+++ src/content/links/rlmCli.md
@@ -0,0 +1,7 @@
+---
+title: "RLM CLI"
+url: "https://github.com/viplismism/rlm-cli"
+date: "03-30-2026"
+---
+
+RLM CLI. I had the thought to make this myself two weeks ago and thought id do it later. Goes to show that being quick is important
